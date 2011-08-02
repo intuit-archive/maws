ff25561b2c10c626d88aec0cc5e4d28804d5f161
marc attinasi <marc@attinasi.org>
2011-08-02 13:50:55 -0700

    expanded scope of start/stop scripts to cover search and services too

--- start_all.sh
+++ start_all.sh
@@ -1,5 +1,8 @@
 ruby lcaws.rb start_apps
 ruby lcaws.rb start_webs
+ruby lcaws.rb start_services
+ruby lcaws.rb start_searches
+
 ruby lcaws.rb start_loadgens
 ruby lcaws.rb validate_servers
 ruby lcaws.rb create_envfile
--- stop_all.sh
+++ stop_all.sh
@@ -1,3 +1,7 @@
 ruby lcaws.rb stop_apps
 ruby lcaws.rb stop_webs
+ruby lcaws.rb stop_services
+ruby lcaws.rb stop_searches
+ruby lcaws.rb stop_caches
+ruby lcaws.rb stop_queues
 ruby lcaws.rb stop_loadgens
