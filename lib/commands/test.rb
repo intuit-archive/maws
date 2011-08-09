b573ada8f24232f516d9ed4d3df5ed5edffb2857
Juozas Gaigalas <jgaigalas@intuit.com>
2011-08-09 14:31:21 -0700

    describe_instances API works in test command

--- lib/commands/test.rb
+++ lib/commands/test.rb
@@ -1,11 +1,17 @@
 require 'lib/command'
-
+require 'awesome_print'
 class Test < Command
   def run!
     puts "test command says 'test'"
-    p @options
-    p @connection
-    p @connection.ec2
-    p @connection.rds
+    # p @options
+    # p @connection
+    # p @connection.ec2
+    # p @connection.rds
+
+    @ec2 = @connection.ec2
+    @rds = @connection.rds
+
+    ap @ec2.describe_instances
+    ap @rds.describe_db_instances
   end
 end
--- maws.rb
+++ maws.rb
@@ -11,12 +11,12 @@ PROFILES_PATH = BASE_PATH + '/config/profiles'
 ROLES_PATH = BASE_PATH + '/config/roles'
 COMMANDS_PATH = BASE_PATH + '/lib/commands'
 
-unless File.exists? "config/aws.key"
-  $stderr.puts "No AWS secret key file: config/aws.key"
+AWS_KEY_FILE = "config/aws.key"
+unless File.exists? AWS_KEY_FILE
+  $stderr.puts "No AWS secret key file: #{AWS_KEY_FILE}"
   exit
 end
-
-KEY_ID,SECRET_KEY = *File.read("config/aws.key").lines
+KEY_ID,SECRET_KEY = *File.read(AWS_KEY_FILE).lines.map {|l| l.chomp}
 
 cp = CommandParser.new(PROFILES_PATH, ROLES_PATH,COMMANDS_PATH)
 command = cp.parse_and_load_command
