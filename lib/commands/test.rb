84e1c4d7d9d08fa0f3823a9078df30f4b844f3cc
Juozas Gaigalas <jgaigalas@intuit.com>
2011-08-15 13:54:24 -0700

    remove some temporary debug code

--- lib/commands/test.rb
+++ lib/commands/test.rb
@@ -14,27 +14,7 @@ class Test < Command
     # ap @rds.describe_db_instances
 
     # puts @ec2.describe_regions
-    debug_instances_list
+    @profile.dump_state
   end
 
-  def debug_instances_list
-    reserved_names = %w(roles lanes)
-    defined_roles = @roles.keys - reserved_names
-    puts defined_roles
-
-    all_roles = @roles.collect{|r| r.name}
-    undefined_roles = defined_roles - all_roles
-
-
-    unknown_roles =
-    roles_list.each do |role|
-      if role.count
-        fixed_counts[role] = role.count
-      end
-    end
-  end
-
-  def symbolize(*names)
-    names.map {|n| n.to_sym}
-  end
 end
