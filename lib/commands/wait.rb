ba613743bc0ea3f7b24e56c647a3b8845bea0a2a
Juozas Gaigalas <jgaigalas@intuit.com>
2011-08-25 11:52:47 -0700

    improved wait command

--- lib/commands/wait.rb
+++ lib/commands/wait.rb
@@ -6,6 +6,9 @@ class Wait < Command
     parser.opt :state, "State to wait for", :type => :string
     parser.opt :wait, "Max wait (seconds)", :default => 60
     parser.opt :count, "Minimum number of instances that must match the state", :default => 0
+    parser.opt :quiet, "Be quiet", :type => :flag, :default => false
+    parser.opt :growl, "Growl notify when done", :type => :flag, :default => false
+
   end
 
   def verify_options
@@ -14,11 +17,19 @@ class Wait < Command
   end
 
   def run!
+    at_exit do
+      if options.growl
+        system("growlnotify -m 'waiting for AWS state #{options.state} done'")
+      end
+    end
+
     state = options.state
     been_waiting = 0
     total_count = specified_instances.count
     wait_for_count = options.count == 0 ? total_count : options.count
+    info "waiting #{options.wait} seconds or until #{wait_for_count}/#{total_count} are #{state}..."
     loop do
+      trap("INT") { info "...done (interrupted)"; return }
       return if been_waiting >= options.wait
       left_to_wait = options.wait - been_waiting
       matching_count = specified_instances.select{|i| i.has_approximate_status?(state)}.count
@@ -27,13 +38,19 @@ class Wait < Command
         return
       end
 
-      info "#{matching_count}/#{total_count} are #{state} - wait #{left_to_wait} seconds or until #{wait_for_count}/#{total_count} are #{state}..."
+      if options.quiet
+        print "."
+        $stdout.flush
+      else
+        info "#{matching_count}/#{total_count} are #{state} - wait #{left_to_wait} seconds or until #{wait_for_count}/#{total_count} are #{state}..."
+      end
+
 
       sleep 1
       been_waiting += 1
       connection.clear_cached_descriptions
+      connection.silent = true
       specified_instances.each {|i| i.sync}
     end
-
   end
 end
