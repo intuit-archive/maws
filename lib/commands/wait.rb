9cea041bb77a6d6cc3674757b0262e9185186ca3
Juozas Gaigalas <jgaigalas@intuit.com>
2011-08-25 11:18:09 -0700

    added minimum count to wait command

--- lib/commands/wait.rb
+++ lib/commands/wait.rb
@@ -5,6 +5,7 @@ class Wait < Command
   def add_specific_options(parser)
     parser.opt :state, "State to wait for", :type => :string
     parser.opt :wait, "Max wait (seconds)", :default => 60
+    parser.opt :count, "Minimum number of instances that must match the state", :default => 0
   end
 
   def verify_options
@@ -15,18 +16,23 @@ class Wait < Command
   def run!
     state = options.state
     been_waiting = 0
+    total_count = specified_instances.count
+    wait_for_count = options.count == 0 ? total_count : options.count
     loop do
       return if been_waiting >= options.wait
-      matching = @selected_instances.map {|i| i.has_approximate_status?(state)}
-      total_count = matching.count
-      true_count = matching.select{|x| x}.count
-      info "#{true_count}/#{total_count} are #{state}"
-      return if total_count == true_count
+      left_to_wait = options.wait - been_waiting
+      matching_count = specified_instances.select{|i| i.has_approximate_status?(state)}.count
+      if matching_count >= wait_for_count
+        info "...done (#{matching_count}/#{total_count} are #{state})"
+        return
+      end
+
+      info "#{matching_count}/#{total_count} are #{state} - wait #{left_to_wait} seconds or until #{wait_for_count}/#{total_count} are #{state}..."
 
       sleep 1
       been_waiting += 1
       connection.clear_cached_descriptions
-      @selected_instances.each {|i| i.sync}
+      specified_instances.each {|i| i.sync}
     end
 
   end
