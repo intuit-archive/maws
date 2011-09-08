95d19cd0b2a0d89cf8a66eb2461c3a0fc50436d9
Juozas Gaigalas <jgaigalas@intuit.com>
2011-09-08 12:40:28 -0700

    elb-remove

--- lib/commands/elb-remove.rb
+++ lib/commands/elb-remove.rb
@@ -3,6 +3,9 @@ require 'lib/trollop'
 
 class ElbRemove < Command
   def run!
-    puts "elb remove!"
+    elbs, instances = partition_elbs_and_instances
+    return if elbs.nil? or instances.nil?
+
+    elbs.each {|elb| elb.remove_instances(instances)}
   end
 end
